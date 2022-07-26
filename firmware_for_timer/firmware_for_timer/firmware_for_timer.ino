6917e95c401449a66fdd8b9119b2348c08f2882e
Nikita Mishkov <mishkov.dd@gmail.com>
2022-07-26 22:35:15 +0300

    Update comments

--- firmware_for_timer/firmware_for_timer/firmware_for_timer.ino
+++ firmware_for_timer/firmware_for_timer/firmware_for_timer.ino
@@ -1,96 +1,120 @@
-#include <Wire.h>                                            //библиотека для работы с шиной i2c
-#include <iarduino_RTC.h>                                    //библиотека для rtc модуля(если cчто, лежит в папке проекта(Timer->firmware_for_timer->iarduino_RTC))     
-#include <EEPROM.h>                                          //библиотека для работы с энергонезависимой памятью
+// ЗАМЕТКИ
+//
+// В последнем байте памяти лежит количество счетчиков
 
-iarduino_RTC time(RTC_DS1307);                               //создание объекта для работы с модулем реального времени
+#include <Wire.h>
+#include <iarduino_RTC.h>
+#include <EEPROM.h>
 
-int values;                                                  //переменная для получения данных
+iarduino_RTC time(RTC_DS1307);
 
-unsigned int to_minutes(unsigned int hours, unsigned int minutes)//функция для перевода часов и минут в минуты
+// переменная для получения данных
+int values;
+
+// функция для перевода часов и минут в минуты
+unsigned int to_minutes(unsigned int hours, unsigned int minutes)
 {
   return (60*hours)+minutes;
 }
 
 void setup()
 {
-  time.begin();                                             //начало работы часов
-  Serial.begin(9600);                                       //открытие сериал порта
+  time.begin();
+  Serial.begin(9600);
   
 }
 
 void loop()
 {
-  if(Serial.available() > 0)                                //если что-то пришло
+  if(Serial.available() > 0)
   { 
     values = Serial.read();
-    time.settime(0,-1, values);                             //считывание и установка часов
+    // считывание и установка часов
+    time.settime(0,-1, values);
+    
     delay(50);
     values = Serial.read();
-    time.settime(0, values);                                //считывание и установка минут
+    // считывание и установка минут
+    time.settime(0, values);
     delay(50);
 
-    //очищаем энергонезависимую память
+    // очищаем энергонезависимую память
     for (int i = 0 ; i < EEPROM.length() ; i++)
     {
       EEPROM.write(i, 0);
     }
-    //////////////////////////////////
     
     values = Serial.read();                                 
     delay(50);
     
-    //запись данных в энергонезависимую память, полученых с телефона
-    while (values > 0)                                        //пока есть что получать
+    // запись данных в энергонезависимую память, полученых с телефона
+    while (values > 0)
     {
+      // 7 - количество параметров одного таймера
       for(int i = 0; i < 7; ++i)
       {
+        // записываем последовательно в память параметры полученного таймера
+        //
+        // EEPROM.length() - 1 - последний байт
+        // 7 - количество параметров одного таймера
         EEPROM.write(EEPROM.read(EEPROM.length() - 1) * 7 + i, Serial.read());
         delay(50);
       }
 
+      // увеличиваем последний байт в памяти на единицу, тем самым увеличивая количество полученный таймеров
       EEPROM.write(EEPROM.length() - 1, EEPROM.read(EEPROM.length() - 1) + 1);
 
+      // читаем следующий полученый байт
       values = Serial.read();
       delay(50);
     }
-    /////////////////////////////////////////
   }
 
-  //проход по каждому таймеру
+  // проход по каждому таймеру
   for(int i = 0; i < EEPROM.read(EEPROM.length() - 1); ++i)
   {
-    if(EEPROM.read(i * 7 + 0) == 1)                                                                               //если таймер нужно проверять
+    // если таймер нужно проверять
+    if(EEPROM.read(i * 7 + 0) == 1)
     {
-      bool find = 0;                                                                                              //флаг
+      bool find = 0;
       time.gettime();
-      if((to_minutes(EEPROM.read(i * 7 + 1), EEPROM.read(i * 7 + 2)) <= to_minutes(time.Hours, time.minutes)) && //если пришло время для работы
+      // если пришло время для работы
+      if((to_minutes(EEPROM.read(i * 7 + 1), EEPROM.read(i * 7 + 2)) <= to_minutes(time.Hours, time.minutes)) &&
          (to_minutes(EEPROM.read(i * 7 + 3), EEPROM.read(i * 7 + 4)) > to_minutes(time.Hours, time.minutes))) 
       {
-        pinMode(EEPROM.read(i * 7 + 5), OUTPUT);                                                                  //настраиваем пин таймера как выход
-        digitalWrite(EEPROM.read(i * 7 + 5), EEPROM.read(i * 7 + 6));                                             //посылаем на пин нужное значение
-      }                           
-      else                                                                                                        //иначе если время не пришло
+        // настраиваем пин таймера как выход
+        pinMode(EEPROM.read(i * 7 + 5), OUTPUT);
+        // посылаем на пин нужное значение
+        digitalWrite(EEPROM.read(i * 7 + 5), EEPROM.read(i * 7 + 6));
+      }
+      // иначе если время не пришло
+      else
       {
-        //ищем таймер с таким же пином
+        // ищем таймер с таким же пином
         for (int j = 0; j < EEPROM.read(EEPROM.length() - 1); ++j)
         {
           if(EEPROM.read(i * 7 + 5) == EEPROM.read(j * 7 + 5) && (i != j) && (EEPROM.read(j * 7 + 0) == 1))
           {
-            //и если нашли таймер с таким же пином и если для него время пришло
+            // и если нашли таймер с таким же пином и если для него время пришло
             if((to_minutes(EEPROM.read(j * 7 + 1), EEPROM.read(j * 7 + 2)) <= to_minutes(time.Hours, time.minutes)) && 
-               (to_minutes(EEPROM.read(j * 7 + 3), EEPROM.read(j * 7 + 4)) > to_minutes(time.Hours, time.minutes))) //если пришло время для работы
+               (to_minutes(EEPROM.read(j * 7 + 3), EEPROM.read(j * 7 + 4)) > to_minutes(time.Hours, time.minutes)))
             {
-              pinMode(EEPROM.read(i * 7 + 5), OUTPUT);                                                             //настраиваем пин таймера как выход
-              digitalWrite(EEPROM.read(i * 7 + 5), EEPROM.read(j * 7 + 6));                                        //посылаем на пин нужное значение
-              find = 1;                                                                                            //поднимаем флаг
+              // настраиваем пин таймера как выход
+              pinMode(EEPROM.read(i * 7 + 5), OUTPUT);
+              // посылаем на пин нужное значение
+              digitalWrite(EEPROM.read(i * 7 + 5), EEPROM.read(j * 7 + 6));
+              find = 1;
             }
           }
         }
 
-        if(find == 0)                                                                                              //если таймер с таким же пином не найден 
+        // если таймер с таким же пином не найден
+        if(find == 0)
         {
-          pinMode(EEPROM.read(i * 7 + 5), OUTPUT);                                                                 //настраиваем пин таймера как выход
-          digitalWrite(EEPROM.read(i * 7 + 5), !EEPROM.read(i * 7 + 6));                                           //посылаем на пин значение протиаоположное нужному
+          // настраиваем пин таймера как выход
+          pinMode(EEPROM.read(i * 7 + 5), OUTPUT);
+          // посылаем на пин значение протиаоположное нужному
+          digitalWrite(EEPROM.read(i * 7 + 5), !EEPROM.read(i * 7 + 6));
         }
       }
     }
