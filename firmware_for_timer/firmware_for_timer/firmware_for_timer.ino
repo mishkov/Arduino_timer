32daec9fd794afeeec81a6b95a30fb70a7c8dae9
Nikita Mishkov <mishkov.dd@gmail.com>
2022-07-27 14:33:58 +0300

    Create debug version

--- firmware_for_timer/firmware_for_timer/firmware_for_timer.ino
+++ firmware_for_timer/firmware_for_timer/firmware_for_timer.ino
@@ -5,8 +5,32 @@
 #include <Wire.h>
 #include <iarduino_RTC.h>
 #include <EEPROM.h>
+#include <SoftwareSerial.h>
 
-iarduino_RTC time(RTC_DS1307);
+class Clock {
+  public:
+    int minutes = 0;
+    int Hours = 0;
+    
+    void begin();
+    void settime(int seconds = -1 , int minutes = -1, int hours = -1);
+    void gettime();
+};
+
+void Clock::begin() {}
+
+void Clock::settime(int seconds = -1 , int minutes = -1, int hours = -1) {
+  if (minutes > 0 ) {
+    this->minutes = minutes;
+  }
+  if (hours > 0) {
+    this->Hours = hours;
+  }
+}
+
+void Clock::gettime() {}
+
+Clock time = Clock();
 
 // переменная для получения данных
 int values;
@@ -17,67 +41,147 @@ unsigned int to_minutes(unsigned int hours, unsigned int minutes)
   return (60*hours)+minutes;
 }
 
+const byte rxPin = 11;
+const byte txPin = 12;
+SoftwareSerial bluetoothSerial(rxPin, txPin);
+
+
 void setup()
 {
   time.begin();
   Serial.begin(9600);
-  
+  bluetoothSerial.begin(9600);
 }
+
+// 1 - hour
+// 2 - minute
+// 3 - timer data
+int nextByteType = 1;
 
 void loop()
 {
-  if(Serial.available() > 0)
+  if(bluetoothSerial.available() > 0)
   { 
-    values = Serial.read();
-    // считывание и установка часов
-    time.settime(0,-1, values);
-    
-    delay(50);
-    values = Serial.read();
-    // считывание и установка минут
-    time.settime(0, values);
-    delay(50);
+    if (nextByteType == 1) {
+      Serial.println("hour");
+      
+      values = bluetoothSerial.read();
+      // считывание и установка часов
+      time.settime(0,-1, values);
 
-    // очищаем энергонезависимую память
-    for (int i = 0 ; i < EEPROM.length() ; i++)
-    {
-      EEPROM.write(i, 0);
+      nextByteType = 2;
+        
+      Serial.println(values);
+      Serial.println();
     }
-    
-    values = Serial.read();                                 
-    delay(50);
-    
-    // запись данных в энергонезависимую память, полученых с телефона
-    while (values > 0)
-    {
-      // 7 - количество параметров одного таймера
-      for(int i = 0; i < 7; ++i)
+    if (nextByteType == 2) {
+      Serial.println("minute");
+      
+      values = bluetoothSerial.read();
+      // считывание и установка минут
+      time.settime(0, values);
+
+      nextByteType = 3;
+      
+      Serial.println(values);
+      Serial.println();
+
+      // очищаем энергонезависимую память
+      for (int i = 0 ; i < EEPROM.length() ; i++)
       {
-        // записываем последовательно в память параметры полученного таймера
-        //
-        // EEPROM.length() - 1 - последний байт
-        // 7 - количество параметров одного таймера
-        EEPROM.write(EEPROM.read(EEPROM.length() - 1) * 7 + i, Serial.read());
-        delay(50);
+        EEPROM.write(i, 0);
       }
+    }
 
-      // увеличиваем последний байт в памяти на единицу, тем самым увеличивая количество полученный таймеров
-      EEPROM.write(EEPROM.length() - 1, EEPROM.read(EEPROM.length() - 1) + 1);
+    int i = 0;
+    while(nextByteType == 3) {
+      while(bluetoothSerial.available() > 0) {
+        values = bluetoothSerial.read();
+        if (values == 255) {
+          nextByteType = 1;
+          return;
+        }
+        Serial.println("data");
+        Serial.println(values);
+        EEPROM.write(EEPROM.read(EEPROM.length() - 1) * 7 + i, values);
 
-      // читаем следующий полученый байт
-      values = Serial.read();
-      delay(50);
+        i++;
+        if (i == 7) {
+          Serial.println("One timer is recieved");
+          Serial.println();
+          
+          i = 0;
+          EEPROM.write(EEPROM.length() - 1, EEPROM.read(EEPROM.length() - 1) + 1);
+        }
+      }
     }
   }
+//      values = bluetoothSerial.read();
+//      if (values == 255) {
+//        nextByteType = 1;
+//        return;
+//      }
+//      while (values >= 0) {
+//        while (i < 7 && values >= 0) {
+//          Serial.println("data");
+//          Serial.println(values);
+//          
+//          EEPROM.write(EEPROM.read(EEPROM.length() - 1) * 7 + i, values);
+//          values = bluetoothSerial.read();
+//          if (values == 255) {
+//            Serial.println("end");
+//            Serial.println();
+//            
+//            nextByteType = 1;
+//            return;
+//          }
+//          i++;
+//        }
+//
+//        Serial.print(" * i = ");
+//        Serial.print(i);
+//        Serial.println(" >= 7");
+//        if (i >= 7) {
+//          Serial.println("One timer is recieved");
+//          Serial.println();
+//          
+//          i = 0;
+//          EEPROM.write(EEPROM.length() - 1, EEPROM.read(EEPROM.length() - 1) + 1);
+//        }
+//
+//        values = bluetoothSerial.read();
+//        if (values == 255) {
+//          Serial.println("end");
+//          Serial.println();
+//          
+//          nextByteType = 1;
+//          return;
+//        }
+//      }
+//    }
+//  }
 
+  delay(500);
+  Serial.print(" * number of timers ");
+  Serial.println(EEPROM.read(EEPROM.length() - 1));
   // проход по каждому таймеру
   for(int i = 0; i < EEPROM.read(EEPROM.length() - 1); ++i)
   {
+    Serial.print(" * is ");
+    Serial.print(i);
+    Serial.print(" active = ");
+    Serial.println(EEPROM.read(i * 7 + 0));
     // если таймер нужно проверять
     if(EEPROM.read(i * 7 + 0) == 1)
     {
       bool find = 0;
       time.gettime();
+      Serial.print(" * ");
+      Serial.print(to_minutes(EEPROM.read(i * 7 + 1), EEPROM.read(i * 7 + 2)));
+      Serial.print(" <= ");
+      Serial.print(to_minutes(time.Hours, time.minutes));
+      Serial.print(" < ");
+      Serial.println(to_minutes(EEPROM.read(i * 7 + 3), EEPROM.read(i * 7 + 4)));
       // если пришло время для работы
       if((to_minutes(EEPROM.read(i * 7 + 1), EEPROM.read(i * 7 + 2)) <= to_minutes(time.Hours, time.minutes)) &&
          (to_minutes(EEPROM.read(i * 7 + 3), EEPROM.read(i * 7 + 4)) > to_minutes(time.Hours, time.minutes))) 
@@ -86,6 +190,10 @@ void loop()
         pinMode(EEPROM.read(i * 7 + 5), OUTPUT);
         // посылаем на пин нужное значение
         digitalWrite(EEPROM.read(i * 7 + 5), EEPROM.read(i * 7 + 6));
+        Serial.print(" * ");
+        Serial.print(EEPROM.read(i * 7 + 5));
+        Serial.print(" = ");
+        Serial.println(EEPROM.read(i * 7 + 6));
       }
       // иначе если время не пришло
       else
@@ -103,6 +211,10 @@ void loop()
               pinMode(EEPROM.read(i * 7 + 5), OUTPUT);
               // посылаем на пин нужное значение
               digitalWrite(EEPROM.read(i * 7 + 5), EEPROM.read(j * 7 + 6));
+              Serial.print(" > ");
+              Serial.print(EEPROM.read(i * 7 + 5));
+              Serial.print(" = ");
+              Serial.println(EEPROM.read(j * 7 + 6));
               find = 1;
             }
           }
