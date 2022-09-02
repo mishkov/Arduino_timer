f1e2eaf8be7df932fdb54ef1c9f7d17790b8ae47
Nikita Mishkov <mishkov.dd@gmail.com>
2022-09-02 16:09:56 +0300

    Fix overflowed bug

--- arduino_timer/lib/home_page/scanning_dialog.dart
+++ arduino_timer/lib/home_page/scanning_dialog.dart
@@ -18,77 +18,86 @@ class ScanningDialog extends StatelessWidget {
     return Column(
       mainAxisSize: MainAxisSize.min,
       children: [
-        BlocBuilder<BluetoothConnectionCubit, BluetoothConnectionState>(
-            builder: (context, state) {
-          return Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Text(
-                  state.isScanning ? 'Busqueda...' : 'Encontrado',
-                  style: const TextStyle(
-                    fontSize: 24,
+        Expanded(
+          child:
+              BlocBuilder<BluetoothConnectionCubit, BluetoothConnectionState>(
+                  builder: (context, state) {
+            return Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Text(
+                    state.isScanning ? 'Busqueda...' : 'Encontrado',
+                    style: const TextStyle(
+                      fontSize: 24,
+                    ),
                   ),
                 ),
-              ),
-              state.devices.isNotEmpty
-                  ? BlocListener<BluetoothConnectionCubit,
-                      BluetoothConnectionState>(
-                      listenWhen: (previous, current) {
-                        return previous.devices.length !=
-                            current.devices.length;
-                      },
-                      listener: (context, state) {
-                        if (availableDevicesListScrollController.hasClients) {
-                          updateLayoutController.layoutUpdater?.call();
-                          availableDevicesListScrollController.animateTo(
-                            availableDevicesListScrollController
-                                .position.maxScrollExtent,
-                            duration: const Duration(seconds: 1),
-                            curve: Curves.ease,
-                          );
-                        }
-                      },
-                      child: BlocBuilder<BluetoothConnectionCubit,
+                Expanded(
+                  child: state.devices.isNotEmpty
+                      ? BlocListener<BluetoothConnectionCubit,
                           BluetoothConnectionState>(
-                        builder: (context, state) {
-                          return CustomScrollView(
-                            controller: availableDevicesListScrollController,
-                            shrinkWrap: true,
-                            slivers: [
-                              SliverListWithControlledLayout(
-                                updateLayoutController: updateLayoutController,
-                                delegate: SliverChildBuilderDelegate(
-                                  (context, index) {
-                                    final device = state.devices[index];
-                                    return AvailableDevice(
-                                      device: device,
-                                      onConnect: () async {
-                                        final connectionProvider = context
-                                            .read<BluetoothConnectionCubit>();
-                                        connectionProvider
-                                            .connectTo(device)
-                                            .then(
-                                          (value) {
-                                            Navigator.pop(context);
+                          listenWhen: (previous, current) {
+                            return previous.devices.length !=
+                                current.devices.length;
+                          },
+                          listener: (context, state) {
+                            if (availableDevicesListScrollController
+                                .hasClients) {
+                              updateLayoutController.layoutUpdater?.call();
+                              availableDevicesListScrollController.animateTo(
+                                availableDevicesListScrollController
+                                    .position.maxScrollExtent,
+                                duration: const Duration(seconds: 1),
+                                curve: Curves.ease,
+                              );
+                            }
+                          },
+                          child: BlocBuilder<BluetoothConnectionCubit,
+                              BluetoothConnectionState>(
+                            builder: (context, state) {
+                              return CustomScrollView(
+                                controller:
+                                    availableDevicesListScrollController,
+                                shrinkWrap: true,
+                                slivers: [
+                                  SliverListWithControlledLayout(
+                                    updateLayoutController:
+                                        updateLayoutController,
+                                    delegate: SliverChildBuilderDelegate(
+                                      (context, index) {
+                                        final device = state.devices[index];
+                                        return AvailableDevice(
+                                          device: device,
+                                          onConnect: () async {
+                                            final connectionProvider =
+                                                context.read<
+                                                    BluetoothConnectionCubit>();
+                                            connectionProvider
+                                                .connectTo(device)
+                                                .then(
+                                              (value) {
+                                                Navigator.pop(context);
+                                              },
+                                            );
                                           },
                                         );
                                       },
-                                    );
-                                  },
-                                  childCount: state.devices.length,
-                                ),
-                              ),
-                            ],
-                          );
-                        },
-                      ),
-                    )
-                  : const Text('Equipos no encontrado'),
-            ],
-          );
-        }),
+                                      childCount: state.devices.length,
+                                    ),
+                                  ),
+                                ],
+                              );
+                            },
+                          ),
+                        )
+                      : const Text('Equipos no encontrado'),
+                ),
+              ],
+            );
+          }),
+        ),
         Align(
           alignment: Alignment.bottomRight,
           child: Padding(
